426f1073209679f1f4c88903d62cad67de9eb13e
Leonardo Pedrosa <leonardopedrosa@zaite15.local>
2018-08-14 11:40:18 -0300

    signup function user

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,17 +1,48 @@
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the AuthProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class AuthProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello AuthProvider Provider');
+  constructor(public http: HttpClient,
+              public afAuth: AngularFireAuth,
+              public afDB: AngularFireDatabase) {
+  
   }
 
+  signupUser(email, password, firstName, lastName): any{    
+    return this.afAuth.auth.createUserWithEmailAndPassword(email, password).then((newUser)=> {
+      return this.afAuth.auth.signInWithEmailAndPassword(email, password).then((authenticatedUser) => {
+        let uid = authenticatedUser.user.uid;
+        let userObject ={
+          uid: uid,
+          registeredDate: Date.now(),
+          name: firstName+ " " +lastName,
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          photoURL: ""
+        };
+
+        newUser.user.updateProfile({
+          displayName: firstName + " " +lastName,
+          photoURL: ""
+        });
+
+        return this.afDB.list('userProfile').update(uid, userObject).then(() => true,
+          error => {
+            throw new Error(error.message);
+        })
+
+        }, error => {
+            throw new Error(error.message);
+        })
+
+        }, error => {
+            throw new Error(error.message);
+      });
+    }
 }
